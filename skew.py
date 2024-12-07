bfdfc883f38613c50727144cae65a11531f637a3
Arnaud Gouel <gouel.arnaud@gmail.com>
2024-12-06 22:04:55 -0800

    Refactor merge method in SkewMinHeap to improve spine merging logic and add getSpine method for spine retrieval

--- skew.py
+++ skew.py
@@ -30,20 +30,72 @@ class SkewMinHeap:
             self.root = self.merge(self.root.left, self.root.right)
             self._size -= 1
             return temp
+        
+    def getSpine(self, root):
+        spine = []
+        current = root
+        while current:
+            spine.append(current)
+            current = current.left
+        return spine
 
     def merge(self, leftTree, rightTree):
         if leftTree is None:
             return rightTree
         if rightTree is None:
             return leftTree
+        
+        leftSpine = self.getSpine(leftTree)
+        rightSpine = self.getSpine(rightTree)
 
-        if leftTree.value > rightTree.value:
-            leftTree, rightTree = rightTree, leftTree
+        i = 0
+        j = 0
+        result = None
+        nextNode = None
+        last = None
 
-        leftTree.left, leftTree.right = leftTree.right, leftTree.left
-        leftTree.right = self.merge(rightTree, leftTree.right)
-        return leftTree
+        while i < len(leftSpine) and j < len(rightSpine):
+            left = leftSpine[i]
+            right = rightSpine[j]
+
+            if left.value <= right.value:
+                nextNode = left
+                i += 1
+            else:
+                nextNode = right
+                j += 1
+            
+            if not result:
+                result = nextNode
+            else:
+                last.left = nextNode
+            last = nextNode
+        
+        while i < len(leftSpine):
+            if not result:
+                result = leftSpine[i]
+            else:
+                last.left = leftSpine[i]
+            last = leftSpine[i]
+            i += 1
+
+        while j < len(rightSpine):
+            if not result:
+                result = rightSpine[j]
+            else:
+                last.left = rightSpine[j]
+            last = rightSpine[j]
+            j += 1
+
+        current = result
+        while current:
+            next = current.left
+            current.left, current.right = current.right, current.left
+            current = next
+
+        return result
     
+
     def print(self):
         print(self._recursivePrint(self.root))
 
